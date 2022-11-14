03350d191d48974e6a8d1c2995aac16bc0c5a0c5
方云 <278636505@qq.com>
2022-11-14 19:19:09 +0800

    chore:开发服务器的基本配置

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,23 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  resolve: {
-    // 设置别名
+  resolve: { // 设置别名
     alias: {
       '@': path.resolve(__dirname, 'src'),
       'comps': path.resolve(__dirname, 'src/components'),
     }
   },
-  plugins: [vue()]
+  plugins: [vue()],
+  server: { // 开发服务器选项
+    host: '127.0.0.1',
+    port: 8080,
+    open: true, // boolean | string 在服务器启动时自动在浏览器中打开应用程序
+    proxy: { // 为开发服务器配置自定义代理规则 对象key值以^开头,将会被解释为RegExp
+      '/api': {
+        target: '你要调的后端接口地址',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })

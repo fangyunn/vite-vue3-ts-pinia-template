6a0b98f1a0015ef389b34a858fff594bcff31951
方云 <278636505@qq.com>
2022-11-14 17:51:21 +0800

    chore:设置路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+// 如果找不到path模块或其相对应的类型声明 输入命令 pnpm install @types/node --save-dev
+import path from 'path'
+
 export default defineConfig({
+  resolve: {
+    // 设置别名
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      'comps': path.resolve(__dirname, 'src/components'),
+    }
+  },
   plugins: [vue()]
 })
